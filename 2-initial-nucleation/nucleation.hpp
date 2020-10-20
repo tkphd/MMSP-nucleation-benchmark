50d286580a7bfd2c7b1b9d86e976c22d1b1065cb
Trevor Keller <trevor.keller@nist.gov>
2020-10-20 13:56:46 -0400

    working implementation of 8.2

---
+++ 2-initial-nucleation/nucleation.hpp
@@ -0,0 +1,11 @@
+// nucleation.hpp
+// Example program for the PFHub Nucleation Benchmark using MMSP
+// Questions/comments to trevor.keller@nist.gov (Trevor Keller)
+
+std::string PROGRAM = "nucleation";
+std::string MESSAGE = "Nucleation model using MMSP";
+
+typedef MMSP::grid<1,double> GRID1D;
+typedef MMSP::grid<2,double> GRID2D;
+typedef MMSP::grid<3,double> GRID3D;
+
---
+++ 2-initial-nucleation/par8.2.hpp
@@ -0,0 +1,10 @@
+// Parameters for PFHub Nucleation Benchmark
+// Questions/comments to trevor.keller@nist.gov (Trevor Keller)
+
+#include <cmath>
+const double meshres = 0.1;
+const double L = 500.0;
+const double df = std::sqrt(2) / 12;
+const double r_star = std::sqrt(2) / (6 * df);
+const double r0 = 2.2 * r_star;
+const double stability = 0.125;
