fa5e6b39810fff55c6a0e746db3027239a6e7c19
Trevor Keller <trevor.keller@nist.gov>
2020-10-20 22:00:29 -0400

    oops, wrong size

--- 3-random-nucleation/par8.3.hpp
+++ 3-random-nucleation/par8.3.hpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 const double meshres = 0.4;
-const double L = 500.0;
+const double L = 1000.0;
 const double df = std::sqrt(2) / 12;
 const double r_star = std::sqrt(2) / (6 * df);
 const double r0 = 2.2 * r_star;
